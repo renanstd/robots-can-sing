21c21ceefa77a9799c187bdfb007f59853677a7c
Renan Santana <renan_ota@hotmail.com>
2021-04-02 15:23:07 -0300

    Adiciona mais expressões

--- tests/lcd-eyes/lcd-eyes.ino
+++ tests/lcd-eyes/lcd-eyes.ino
@@ -6,12 +6,34 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 byte eye_open[8] = {
   B00000,
-  B11110,
-  B11110,
-  B11110,
-  B11110,
-  B11000,
-  B11110,
+  B01110,
+  B01110,
+  B01110,
+  B01110,
+  B01000,
+  B01110,
+  B00000,
+};
+
+byte eye_closed[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B00000,
+  B00000,
+  B00000,
+};
+
+byte eye_happy[8] = {
+  B00000,
+  B00000,
+  B01110,
+  B10001,
+  B10001,
+  B10001,
+  B10001,
   B00000,
 };
 
@@ -20,6 +42,8 @@ void setup() {
   lcd.setBacklight(HIGH);
 
   lcd.createChar(0, eye_open);
+  lcd.createChar(1, eye_closed);
+  lcd.createChar(2, eye_happy);
 
   lcd.setCursor(5, 0);
   lcd.write(byte(0));
@@ -28,6 +52,15 @@ void setup() {
   lcd.setCursor(5,1);
   lcd.print("-----");
 
+  delay(3000);
+  
+  lcd.clear();
+  lcd.setCursor(5, 0);
+  lcd.write(byte(2));
+  lcd.setCursor(10, 0);
+  lcd.write(byte(2));
+  lcd.setCursor(5,1);
+  lcd.print("  w");
 }
 
 void loop() {
