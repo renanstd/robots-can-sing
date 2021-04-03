08c8910ce5dbb51ae54c3a40c7b1699c0ce3ca34
Renan Santana <renan_ota@hotmail.com>
2021-04-03 16:41:07 -0300

    Ajusta animação do timmy

--- back_vocal/timmy/timmy.ino
+++ back_vocal/timmy/timmy.ino
@@ -10,7 +10,6 @@ Servo servo;
 
 void setup(){
   servo.attach(3);
-  servo.write(90);
   Wire.begin();
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.setTextColor(WHITE);
@@ -30,7 +29,7 @@ void loop() {
   display.drawLine(60, 31, 70, 31, WHITE);
   display.display();
   delay(3000);
-  
+
   display.clearDisplay();
   display.fillRect(30, 12, 10, 2, WHITE);
   display.fillRect(90, 12, 10, 2, WHITE);
@@ -45,8 +44,17 @@ void loop() {
   display.fillRect(90, 18, 6, 4, BLACK);
   display.drawLine(60, 31, 70, 31, WHITE);
   display.display();
-  delay(3000);
-  
+  delay(2000);
+
+  display.clearDisplay();
+  display.fillRect(30, 8, 10, 20, WHITE);
+  display.fillRect(90, 8, 10, 20, WHITE);
+  display.fillRect(32, 18, 6, 4, BLACK);
+  display.fillRect(92, 18, 6, 4, BLACK);
+  display.drawLine(60, 31, 70, 31, WHITE);
+  display.display();
+  delay(800);
+
   display.clearDisplay();
   display.fillRect(30, 12, 10, 2, WHITE);
   display.fillRect(90, 12, 10, 2, WHITE);
@@ -54,22 +62,29 @@ void loop() {
   display.display();
   delay(50);
 
-//  servo.write(100);
-//  display.clearDisplay();
-//  display.fillRect(30, 12, 10, 2, WHITE);
-//  display.fillRect(90, 12, 10, 2, WHITE);
-//  display.fillCircle(65, 25, 4, WHITE);
-//  display.display();
-  
-//  for (int i=3136; i>2093; i--) {
-//    tone(11, i);
-//    delay(0.3);
-//  }
-//  for (int i=2093; i<3136; i++) {
-//    tone(11, i);
-//    delay(0.3);
-//  }
+  servo.write(100);
+  display.clearDisplay();
+  display.fillRect(30, 12, 10, 2, WHITE);
+  display.fillRect(90, 12, 10, 2, WHITE);
+  display.fillCircle(65, 25, 4, WHITE);
+  display.display();
 
-//  noTone(11);
-//  delay(300); 
+  for (int i=3136; i>2093; i--) {
+    tone(11, i);
+    delay(0.3);
+  }
+  
+  display.clearDisplay();
+  display.fillRect(30, 12, 10, 2, WHITE);
+  display.fillRect(90, 12, 10, 2, WHITE);
+  display.fillCircle(65, 25, 6, WHITE);
+  display.display();
+  
+  for (int i=2093; i<3136; i++) {
+    tone(11, i);
+    delay(0.3);
+  }
+  
+  noTone(11);
+  delay(300);
 }
